03ec6a835b4b0f2adc30edcc9e630fd087a0a462
KBAWX <kbawto@gmail.com>
2021-01-21 09:12:32 +0800

    add note.sql eventsPage.html eventsPage.css

---
+++ note.sql
@@ -0,0 +1,69 @@
+CREATE DATABASE c13_project_1_group_5;
+
+\c c13_project_1_group_5
+
+CREATE TABLE users(
+    id SERIAL primary key,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    gender VARCHAR(255),
+    level INTEGER,
+    experience INTEGER,
+    introduction TEXT,
+    rating INTEGER,
+    user_icon VARCHAR(255),
+    image VARCHAR(255)
+);
+
+CREATE TABLE hiking_trail(
+    id SERIAL primary key,
+    name VARCHAR(255),
+    image VARCHAR(255),
+    introduction TEXT,
+    hardness INTEGER
+);
+
+CREATE TABLE event(
+    id SERIAL primary key,
+    event_name VARCHAR(255),
+    meeting_point VARCHAR(255),
+    date DATE,
+    time TIME,
+    max_number_of_member INTEGER,
+    joining_number_of_member INTEGER,
+    hiking_trail_id INTEGER,
+    FOREIGN KEY (hiking_trail_id) REFERENCES hiking_trail(id),
+    detail TEXT,
+    is_active BOOLEAN
+);
+
+CREATE TABLE chatroom(
+    id SERIAL primary key,
+    users_id INTEGER,
+    FOREIGN KEY (users_id) REFERENCES users(id),
+    event_id INTEGER,
+    FOREIGN KEY (event_id ) REFERENCES event(id),
+    content TEXT
+);
+
+CREATE TABLE user_joining_event(
+    id SERIAL primary key,
+    users_id INTEGER,
+    FOREIGN KEY (users_id) REFERENCES users(id),
+    event_id INTEGER,
+    FOREIGN KEY (event_id ) REFERENCES event(id)
+);
+
+CREATE TABLE rating_event(
+    id SERIAL primary key,
+    users_id INTEGER,
+    FOREIGN KEY (users_id) REFERENCES users(id),
+    event_id INTEGER,
+    FOREIGN KEY (event_id) REFERENCES event(id),
+    rating_person_id INTEGER,
+    FOREIGN KEY (rating_person_id) REFERENCES event(id),
+    rating  INTEGER,
+    comment TEXT
+);
+
